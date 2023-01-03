cfe0c072c3687efc77cb4713bc7ca076a928c1cc
RaulMolinaBadia <21rmolina@ibadia.cat>
2023-01-03 17:40:40 +0100

    refactor of part 2

--- src/components/CardDefault/index.jsx
+++ src/components/CardDefault/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Card, Name, Image, Email, Surname } from './styles'
-export const CardDefault = props => {
+export const CardDefault = ({key, name, surname, email, img}) => {
   return (
-    <Card>
-      <Image src={props.props.card.img.src} alt={props.props.card.img.alt} />
-      <Surname>
-        {props.props.card.name} {props.props.card.surname}
-      </Surname>
-      <Email>{props.props.card.email}</Email>
+    <Card key={key}>
+      <Image src={img} alt={name} />
+      <Name>
+        {name} {surname}
+      </Name>
+      <Email>{email}</Email>
     </Card>
   )
 }
